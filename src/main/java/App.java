08bfb0a4ed0c3d6b668f02f9841ddd3cdaa55a00
Courtney Phillips <courtney@epicodus.com>
2016-09-09 14:01:36 -0700

    Create ArrayList in /tasks route in order to stor emultiple tasks in session data.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,15 +17,20 @@ public class App {
     }, new VelocityTemplateEngine());
 
     get("/tasks", (request, response) -> {
-      Map<String, Object> model = new HashMap<String, Object>();
+    Map<String, Object> model = new HashMap<String, Object>();
 
-      String description = request.queryParams("description");
-      Task newTask = new Task(description);
-      request.session().attribute("task", newTask);
+    ArrayList<Task> tasks = request.session().attribute("tasks");
+    if (tasks == null) {
+      tasks = new ArrayList<Task>();
+      request.session().attribute("tasks", tasks);
+    }
 
-      model.put("template", "templates/success.vtl");
-      return new ModelAndView(model, layout);
-    }, new VelocityTemplateEngine());
+    String description = request.queryParams("description");
+    Task newTask = new Task(description);
+    tasks.add(newTask);
 
+    model.put("template", "templates/success.vtl");
+    return new ModelAndView(model, layout);
+   }, new VelocityTemplateEngine());
   }
 }
