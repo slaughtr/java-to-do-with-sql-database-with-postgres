d8ac5565f86b64452f0633b67b9c7205d0486c4b
Courtney Phillips <courtney@epicodus.com>
2016-09-09 14:03:24 -0700

    Import ArrayList class in App.java.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,7 @@
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
