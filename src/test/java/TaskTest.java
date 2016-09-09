c272d309f9813e25792e7950bedd5c7be650bda1
Courtney Phillips <courtney@epicodus.com>
2016-09-09 12:59:12 -0700

    Write test asserting that we can create new instance of Task.

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -3,4 +3,10 @@ import static org.junit.Assert.*;
 
 public class TaskTest {
 
+  @Test
+  public void Task_instantiatesCorrectly_true() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(true, myTask instanceof Task);
+  }
+
 }
