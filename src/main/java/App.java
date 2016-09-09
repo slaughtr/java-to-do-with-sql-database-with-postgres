8dd215dda60f1d099463d819acce7cb624a2d2aa
Courtney Phillips <courtney@epicodus.com>
2016-09-09 14:01:58 -0700

    Fetch ArrayList of tasks in root path.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,7 +11,7 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("task", request.session().attribute("task"));
+      model.put("task", request.session().attribute("tasks"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
