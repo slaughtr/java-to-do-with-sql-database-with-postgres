648d42f69a551663bc8d9a39b8ca4fc3fe45ba0b
Courtney Phillips <courtney@epicodus.com>
2016-09-09 13:06:01 -0700

    Declare mDescription; property, assign in constructor, and retrieve with dedicated getter method.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,12 @@
 public class Task {
+  private String mDescription;
 
   public Task(String description) {
+    mDescription = description;
+  }
+
+  public String getDescription() {
+    return mDescription;
   }
 
 }
