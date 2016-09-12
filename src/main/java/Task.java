abb03fef5d53fb81538abb0962c072df84fbbf8b
Courtney Phillips <courtney@epicodus.com>
2016-09-12 15:21:09 -0700

    Add mCompleted property to Task class, including corresponding getter method. Set to begin as false by default in constructor.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,18 @@
 public class Task {
   private String mDescription;
+  private boolean mCompleted;
 
   public Task(String description) {
     mDescription = description;
+    mCompleted = false;
   }
 
   public String getDescription() {
     return mDescription;
+  }
+
+  public boolean isCompleted() {
+    return mCompleted;
   }
 
 }
