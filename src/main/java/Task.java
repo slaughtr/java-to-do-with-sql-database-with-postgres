69a1925c7d12ddb099644e68293faebff271528e
Courtney Phillips <courtney@epicodus.com>
2016-09-12 15:30:12 -0700

    Create static ArrayList to contain all instances of Task class. Add individual Task objects to ArrayList in logic for Task constructor. Include getter method to return ArrayList of all Tasks.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,11 +4,13 @@ public class Task {
   private String mDescription;
   private boolean mCompleted;
   private LocalDateTime mCreatedAt;
+  private static ArrayList<Task> instances = new ArrayList<Task>();
 
   public Task(String description) {
     mDescription = description;
     mCompleted = false;
     mCreatedAt = LocalDateTime.now();
+    instances.add(this);
   }
 
   public String getDescription() {
@@ -22,5 +24,9 @@ public class Task {
   public LocalDateTime getCreatedAt() {
     return mCreatedAt;
   }
+
+  public static ArrayList<Task> all() {
+   return instances;
+ }
 
 }
