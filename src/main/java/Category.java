02296520c4ff451a878971079cc5e3fb62beacb5
Courtney Phillips <courtney@epicodus.com>
2016-09-13 21:20:43 -0700

    Fix error in Category class; mId was not declared as int.

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 public class Category {
   private String mName;
   private static ArrayList<Category> instances = new ArrayList<Category>();
+  private int mId;
 
   public Category(String name) {
     mName = name;
     instances.add(this);
+    mId = instances.size();
   }
 
   public String getName() {
@@ -21,5 +23,9 @@ public class Category {
  public static void clear() {
    instances.clear();
  }
+
+ public int getId() {
+    return mId;
+  }
 
 }
